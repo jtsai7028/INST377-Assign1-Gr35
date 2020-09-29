13dbef7e9c2fb33729170583594778813a9de02a
Jessica Tsai <jess.tsai7028@gmail.com>
2020-09-28 21:07:25 -0400

    fixing heroku get

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('INST377-Assign1-Gr35'));
 
 app.route('/index')
